9899eac5642f749c3499e270a65ad1c7c4dfb3e5
maxdacho <92314290+maxdacho@users.noreply.github.com>
2022-11-09 10:46:37 +0100

    Arbeiten am Design Grundgerüst

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -7,7 +7,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-    <h1 class="bg-primary">Hello, world!</h1>
+    <div class="container-fluid bg-warning">
+    <h1>Laravel Notebook App</h1>
+</div>
+<div class="container col-9 my-5" style="display:flex; justify-content:space-between">
+  <div>Deine Notizb&uuml;cher</div>
+  <button class="btn btn-success">Notizbuch anlegen</button>
+</div>
+
+<div class="container col-9 my-5" style="display:flex; justify-content:space-between">
+<div class="notebook-item" style="border: 1px solid #000; padding:2rem;">
+  Notebook 1
+  <br>
+    <button class="btn btn-primary">Edit</button>
+    <button class="btn btn-danger">Delete</button>
+</div>
+
+<div class="notebook-item" style="border: 1px solid #000; padding:2rem;">
+Notebook 2
+<br>
+  <button class="btn btn-primary">Edit</button>
+  <button class="btn btn-danger">Delete</button>
+</div>
+
+<div class="notebook-item" style="border: 1px solid #000; padding:2rem;">
+Notebook 3
+<br>
+  <button class="btn btn-primary">Edit</button>
+  <button class="btn btn-danger">Delete</button>
+</div>
+
+<div class="notebook-item" style="border: 1px solid #000; padding:2rem;">
+Notebook 4
+<br>
+  <button class="btn btn-primary">Edit</button>
+  <button class="btn btn-danger">Delete</button>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
