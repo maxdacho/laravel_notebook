20121186d6524fbf5642d2e2bbf12abba1018c19
maxdacho <92314290+maxdacho@users.noreply.github.com>
2022-11-25 09:54:22 +0100

    Finale Abgabe

--- resources/views/notebooks/index.blade.php
+++ resources/views/notebooks/index.blade.php
@@ -2,15 +2,14 @@
 <html lang="de">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Notebook App</title>
-  <link href="/css/general.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-</head>
-
-<body>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Notebook App</title>
+    <link href="/css/general.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  </head>
+  <body>
   <div class="container-fluid bg-warning" style="display: flex; justify-content: space-between;">
     <h1>Laravel Notebook App</h1>
     <div class="my-3">
@@ -21,52 +20,51 @@
       <br>
       <a href="/register">Registrieren</a>
       @endif
-    </div>
-    <div class="my-2">
-      <a href="{{url('/logout')}}" onclick="event.preventDefault();
+      </div>
+      <div class="my-2">
+      <a href="{{url('/logout')}}"
+        onclick="event.preventDefault();
         document.getElementById('logut-form').submit();">
         <button class="btn btn-danger">Ausloggen</button>
       </a>
-    </div>
-    <form id="logut-form" action="{{url('/logout')}}" method="POST">
+      </div>
+      <form id="logut-form" action="{{url('/logout')}}" method="POST">
       {{csrf_field()}}
-    </form>
-  </div>
-  <div class="container col-9 my-5" style="display:flex; justify-content:space-between">
-    <div>
-      <h2><b>Ihre Notizb&uuml;cher</b></h2>
-    </div>
-    <div class="search">
-      <input type="search" name="search" id="search" placeholder="Nach Notizen suchen" class="form-control">
+      </form>
+</div>
+<div class="container col-9 my-5" style="display:flex; justify-content:space-between">
+  <div><h2><b>Ihre Notizb&uuml;cher</b></h2></div>
+  <div class="search">
+    <input type="search" name="search" id="search" placeholder="Nach Notizen suchen" class="form-control">  
     </div>
     <a href="/notebooks/create">
-      <button class="btn btn-success">Notizbuch anlegen</button>
-    </a>
-  </div>
+  <button class="btn btn-success">Notizbuch anlegen</button>
+  </a>
+</div>
 
 
-  <div class="container col-9 my-5" style="display:flex; flex-wrap:wrap; justify-content:center;">
-    <div class="alldata" style="display:flex; flex-wrap:wrap; justify-content:center;">
-      @foreach($notebooks as $notebook)
-      <div class="notebook-item" style="border-radius:10px; background:white; padding:4rem; margin:0.5rem;">
-        <a href="{{route("notebooks.show",$notebook->id)}}">
-          <h1 class="notebook-title">
-            {{$notebook->name}}
-          </h1>
-        </a>
-        <a href="{{route("notebooks.edit",$notebook->id)}}">
-          <button class="btn btn-primary my-3">Bearbeiten</button>
-        </a>
-        <form action="/notebooks/{{$notebook->id}}" method="POST">
-          {{csrf_field()}}
-          {{method_field('DELETE')}}
-          <input class="btn btn-danger" type="submit" value="Entfernen">
-        </form>
+<div class="container col-9 my-5" style="display:flex; flex-wrap:wrap; justify-content:center;">
+  <div class="alldata" style="display:flex; flex-wrap:wrap; justify-content:center;">
+  @foreach($notebooks as $notebook) 
+    <div class="notebook-item" style="border-radius:10px; background:white; padding:4rem; margin:0.5rem;">
+      <a href="{{route("notebooks.show",$notebook->id)}}">
+      <h1 class="notebook-title">
+      {{$notebook->name}}
+      </h1>
+      </a>
+      <a href="{{route("notebooks.edit",$notebook->id)}}">
+        <button class="btn btn-primary my-3">Bearbeiten</button>
+      </a>
+      <form action="/notebooks/{{$notebook->id}}" method="POST">
+      {{csrf_field()}}
+      {{method_field('DELETE')}}
+        <input class="btn btn-danger" type="submit" value="Entfernen">
+      </form>
       </div>
       @endforeach
-    </div>
+</div>
 
-    <div id="content" class="searchdata" style="display:flex; flex-wrap:wrap; justify-content:center;"></div>
+<div id="content" class="searchdata" style="display:flex; flex-wrap:wrap; justify-content:center;"></div>
 
 
 
@@ -79,34 +77,30 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script type="text/javascript">
-    $('#search').on('keyup', function() {
-      $value = $(this).val();
-      if ($value) {
-        $('.alldata').hide();
-        console.log($('.alldata'));
-        $('.searchdata').show();
-      } else {
-        $('.alldata').show();
-        $('.searchdata').hide();
-      }
-
-
-      $.ajax({
-        type: 'get',
-        url: '{{URL::to('
-        search ')}}',
-        data: {
-          'search': $value
-        },
-
-        success: function(data) {
-          console.log(data);
-          $('#content').html(data);
+    $('#search').on('keyup',function()
+    {
+        $value=$(this).val();
+        if($value){
+          $('.alldata').hide();
+          console.log($('.alldata'));
+          $('.searchdata').show();
+        }
+        else{
+          $('.alldata').show();
+          $('.searchdata').hide();
         }
 
-      });
-    })
-  </script>
-</body>
+        $.ajax({
+            type: 'get',
+            url: '{{URL::to('search')}}',
+            data: {'search':$value},
 
+            success:function(data){
+              console.log(data);
+              $('#content').html(data);
+            }
+          });
+        })
+          </script>
+  </body>
 </html>
